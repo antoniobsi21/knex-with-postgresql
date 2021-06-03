5c5ede28beb325028cd8219ab56e6fca209fbbb4
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2021-06-03 13:39:54 -0300

    seeds added

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,13 @@ module.exports = {
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
     }
   }
 };
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv/config');
 
 app.get('/', (req, res) => {
-    res.send('Welcome');
+  res.send('Welcome');
 });
 
 const PORT = process.env.PORT;
